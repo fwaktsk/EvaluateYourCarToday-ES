4ea0b244d200b6c1ca00c65bdfb3fa7bfaf04927
Greg <gregorius.lai@student.umn.ac.id>
2022-11-28 11:44:22 +0700

    Styling bootstrap

--- web/index.php
+++ web/index.php
@@ -5,47 +5,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <title>Home</title>
 </head>
 
 <body>
-    <form action="result.php" method="get">
-            <label for="price">Car price:</label>
-            <select id="price" name="price">
+    <div class="d-flex justify-content-center">
+    <form class="col-4 m-4 p-3 shadow rounded bg-light" action="result.php" method="get">
+            <label class="form-label" for="price">Car price:</label>
+            <select class="form-select" id="price" name="price">
                 <option value="high">High</option>
                 <option value="med">Medium</option>
                 <option value="low">Low</option>
             </select>
         <br>
-            <label for="tech">Technology:</label>
-            <select id="tech" name="tech">
+            <label class="form-label" for="tech">Technology:</label>
+            <select class="form-select" id="tech" name="tech">
                 <option value="high">High</option>
                 <option value="med">Medium</option>
                 <option value="low">Low</option>
             </select>
         <br>
-            <label for="doors">Number of Doors: </label>
-            <input type="number" name="doors">
+            <label  class="form-label" for="doors">Number of Doors: </label>
+            <input  class="form-control" type="number" name="doors" placeholder="Number of Doors">
         <br>
-            <label for="people">Seat Capacity: </label>
-            <input type="number" name="people">
+            <label class="form-label" for="people">Seat Capacity: </label>
+            <input class="form-control" type="number" name="people" placeholder="Seat Capacity">
         <br>
-            <label for="luggage">Luggage Size:</label>
-            <select id="luggage" name="luggage">
+            <label class="form-label" for="luggage">Luggage Size:</label>
+            <select class="form-select" id="luggage" name="luggage">
                 <option value="big">Big</option>
                 <option value="med">Medium</option>
                 <option value="small">Small</option>
             </select>
         <br>
-            <label for="security">Security:</label>
-            <select id="security" name="security">
+            <label class="form-label" for="security">Security:</label>
+            <select class="form-select" id="security" name="security">
                 <option value="high">High</option>
                 <option value="med">Medium</option>
                 <option value="low">Low</option>
             </select>
         <br>
-            <input type="submit">
+            <input class="btn btn-primary" type="submit">
     </form>
+    </div>
 </body>
 
 </html>
